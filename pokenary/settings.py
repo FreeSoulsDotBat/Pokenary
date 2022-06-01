4a7a1367d3cf266f16c12c6b7a931d5ec86e536e
jonatan.bossan <jonatan.bossan@akna.com>
2022-06-01 19:55:18 -0300

    Compress enabled

--- pokenary/settings.py
+++ pokenary/settings.py
@@ -140,9 +140,7 @@ COMPRESS_PRECOMPILERS = [
     ('text/x-scss', 'django_libsass.SassCompiler'),
 ]
 
-# COMPRESS_ENABLED = True
-
-COMPRESS_OFFLINE = True
+COMPRESS_ENABLED = True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
