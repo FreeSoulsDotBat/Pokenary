62575e44d67be9f1a29245f9b3c076926c23a966
jonatan.bossan <jonatan.bossan@akna.com>
2022-06-01 20:37:38 -0300

    compress set up

--- pokenary/settings.py
+++ pokenary/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 
 STATIC_URL = '/static'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_main')]
 
@@ -139,8 +139,6 @@ STATICFILES_FINDERS = [
 COMPRESS_PRECOMPILERS = [
     ('text/x-scss', 'django_libsass.SassCompiler'),
 ]
-
-COMPRESS_ENABLED = True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
