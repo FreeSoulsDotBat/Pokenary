4abf09f2632b7e2a9052cf8197cb877596a78dea
jonatan.bossan <jonatan.bossan@akna.com>
2022-06-04 00:10:48 -0300

    Settings

--- pokenary/settings.py
+++ pokenary/settings.py
@@ -26,6 +26,8 @@ SECRET_KEY = 'django-insecure-^p52-_%y9x4#91p67lt7f0ckt3r80^h=w(hx&a3rwo8(qx7kkt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+ADMINS = [('jonatan.fbossan@gmail.com')]
+
 ALLOWED_HOSTS = ['pokenary.herokuapp.com']
 
 
